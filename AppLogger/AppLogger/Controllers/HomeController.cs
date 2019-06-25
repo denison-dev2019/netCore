ee8911f0866d1b49e8c02ee6d29585a6f5c96107
denison-dev2019 <52185601+denison-dev2019@users.noreply.github.com>
2019-06-25 01:16:40 -0300

    Add files via upload

---
+++ AppLogger/AppLogger/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AppLogger.Models;
+using Serilog;
+
+namespace AppLogger.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+
+            Log.Logger = new LoggerConfiguration()
+               .MinimumLevel.Debug()
+               .WriteTo.Console()
+               .WriteTo.File("logs\\meuLogapp.txt", rollingInterval: RollingInterval.Day)
+               .CreateLogger();
+
+            Log.Information("Olá, Serilog !");
+
+            int a = 10, b = 0;
+            var teste = 0;
+            try
+            {
+                Log.Debug("Dividindo {A} por {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Alguma coisa deu errado...");
+            }
+
+            Log.CloseAndFlush();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
